696c7864cbb4b7de3522b93b3465cd7b9e306c79
Elias Tantu <ellatantu@gmail.com>
2023-09-15 15:10:15 +0300

    first commit

---
+++ src/features/notes/NewNoteForm.js
@@ -0,0 +1,204 @@
+import { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import { useAddNewNoteMutation } from "./notesApiSlice";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faSave } from "@fortawesome/free-solid-svg-icons";
+
+const initialState = {
+  Branch_Name: "",
+  Item_Type: "",
+  Model: "",
+  Serial_Number: "",
+  Problem: "",
+  Required_Equipments: "",
+};
+
+const NewNoteForm = ({ users }) => {
+  const [addNewNote, { isLoading, isSuccess, isError, error }] =
+    useAddNewNoteMutation();
+
+  const navigate = useNavigate();
+
+  const [values, setValues] = useState(initialState);
+  //const [text, setText] = useState({});
+  const [userId, setUserId] = useState(users[0].id);
+
+  useEffect(() => {
+    if (isSuccess) {
+      setValues({});
+      //setText({});
+      setUserId("");
+      navigate("/dash/notes");
+    }
+  }, [isSuccess, navigate]);
+
+  const handelChange = (e) =>
+    setValues({
+      ...values,
+      [e.target.name]: e.target.value,
+    });
+  //const onTextChanged = (e) => setText(e.target.value);
+  const onUserIdChanged = (e) => setUserId(e.target.value);
+
+  const canSave = [values.Branch_Name, userId].every(Boolean) && !isLoading;
+
+  const onSaveNoteClicked = async (e) => {
+    e.preventDefault();
+    if (canSave) {
+      await addNewNote({ Checked_By: userId, ...values });
+    }
+  };
+
+  const options = users.map((user) => {
+    return (
+      <option key={user.id} value={user.id}>
+        {" "}
+        {user.username}
+      </option>
+    );
+  });
+
+  const errClass = isError ? "errmsg" : "offscreen";
+  const validTitleClass = !values.Branch_Name ? "form__input--incomplete" : "";
+  //const validTextClass = !text ? "form__input--incomplete" : "";
+
+  const content = (
+    <>
+      <p className={errClass}>{error?.data?.message}</p>
+
+      <form className="form" onSubmit={onSaveNoteClicked}>
+        <div className="form__title-row">
+          <h2>New Computer</h2>
+          <div className="form__action-buttons">
+            <button className="icon-button" title="Save" disabled={!canSave}>
+              <FontAwesomeIcon icon={faSave} />
+            </button>
+          </div>
+        </div>
+        <label className="form__label" htmlFor="title">
+          Branch Name:
+        </label>
+        <input
+          className={`form__input ${validTitleClass}`}
+          id="title"
+          name="Branch_Name"
+          type="text"
+          autoComplete="off"
+          value={values.Branch_Name}
+          onChange={handelChange}
+        />
+        <label className="form__label" htmlFor="title">
+          Item Type:
+        </label>
+        <input
+          className={`form__input ${validTitleClass}`}
+          id="title"
+          name="Item_Type"
+          type="text"
+          autoComplete="off"
+          value={values.Item_Type}
+          onChange={handelChange}
+        />
+        <label className="form__label" htmlFor="title">
+          Model:
+        </label>
+        <input
+          className={`form__input ${validTitleClass}`}
+          id="title"
+          name="Model"
+          type="text"
+          autoComplete="off"
+          value={values.Model}
+          onChange={handelChange}
+        />
+        <label className="form__label" htmlFor="title">
+          Serial No:
+        </label>
+        <input
+          className={`form__input ${validTitleClass}`}
+          id="title"
+          name="Serial_Number"
+          type="text"
+          autoComplete="off"
+          value={values.Serial_Number}
+          onChange={handelChange}
+        />
+        <label className="form__label" htmlFor="title">
+          Problem:
+        </label>
+        <input
+          className={`form__input ${validTitleClass}`}
+          id="title"
+          name="Problem"
+          type="text"
+          autoComplete="off"
+          value={values.Problem}
+          onChange={handelChange}
+        />
+
+        <label className="form__label" htmlFor="text">
+          Required Equipments:
+        </label>
+        <textarea
+          className={`form__input form__input--text {validTextClass}`}
+          id="text"
+          name="Required_Equipments"
+          value={values.Required_Equipments}
+          onChange={handelChange}
+        />
+
+        <label
+          className="form__label form__checkbox-container"
+          htmlFor="username"
+        >
+          checked By:
+        </label>
+        <select
+          id="username"
+          name="username"
+          className="form__select"
+          value={userId}
+          onChange={onUserIdChanged}
+        >
+          {options}
+        </select>
+
+        <label
+          className="form__label form__checkbox-container"
+          htmlFor="username"
+        >
+          Mantained By:
+        </label>
+        <select
+          id="username"
+          name="username"
+          className="form__select"
+          value={userId}
+          onChange={onUserIdChanged}
+        >
+          {options}
+        </select>
+
+        <label
+          className="form__label form__checkbox-container"
+          htmlFor="username"
+        >
+          ASSIGNED TO:
+        </label>
+        <select
+          id="username"
+          name="username"
+          className="form__select"
+          value={userId}
+          onChange={onUserIdChanged}
+        >
+          {options}
+        </select>
+      </form>
+    </>
+  );
+
+  return content;
+};
+
+export default NewNoteForm;
---
+++ src/features/notes/Note.js
@@ -0,0 +1,59 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faPenToSquare } from "@fortawesome/free-solid-svg-icons";
+import { useNavigate } from "react-router-dom";
+import { useGetNotesQuery } from "./notesApiSlice";
+import { memo } from "react";
+
+const Note = ({ noteId }) => {
+  const { note } = useGetNotesQuery("notesList", {
+    selectFromResult: ({ data }) => ({
+      note: data?.entities[noteId],
+    }),
+  });
+  //console.log(note);
+  const navigate = useNavigate();
+
+  if (note) {
+    const created = new Date(note.createdAt).toLocaleString("en-US", {
+      day: "numeric",
+      month: "long",
+    });
+
+    const updated = new Date(note.updatedAt).toLocaleString("en-US", {
+      day: "numeric",
+      month: "long",
+    });
+
+    const handleEdit = () => navigate(`/dash/notes/${noteId}`);
+
+    return (
+      <tr className="table__row">
+        <td className="table__cell note__status">
+          {note.completed ? (
+            <span className="note__status--completed">Completed</span>
+          ) : (
+            <span className="note__status--open">Open</span>
+          )}
+        </td>
+        <td className="table__cell note__created">{created}</td>
+        <td className="table__cell note__updated">{updated}</td>
+        <td className="table__cell note__branchName">{note.Branch_Name}</td>
+        <td className="table__cell note__username">{note.username}</td>
+        <td className="table__cell note__itemType">{note.Item_Type}</td>
+        <td className="table__cell note__model">{note.Model}</td>
+        <td className="table__cell note__serialNumber">{note.Serial_Number}</td>
+        <td className="table__cell note__problem">{note.Problem}</td>
+
+        <td className="table__cell">
+          <button className="icon-button table__button" onClick={handleEdit}>
+            <FontAwesomeIcon icon={faPenToSquare} />
+          </button>
+        </td>
+      </tr>
+    );
+  } else return null;
+};
+
+const memoizedNote = memo(Note);
+
+export default memoizedNote;
